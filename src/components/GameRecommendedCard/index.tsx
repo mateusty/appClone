9e8cb682ddc030c5e7f6261c677234e7600d88ab
mateusty <mateustamaki43@gmail.com>
2026-06-17 15:21:26 -0300

    Arrumando lógica no card de recomendação

--- src/components/GameRecommendedCard/index.tsx
+++ src/components/GameRecommendedCard/index.tsx
@@ -15,7 +15,7 @@ export const GameRecommendedCard = ({ game }: GameRecommendedCardProps) => {
       colors={['#0F151C', '#172531','#1D3750', '#1b3549']}
       style={styles.cardGradient}
       >
-        <Text style={[styles.cardText, {fontWeight: '600', fontSize: 24}]}>{game.title.length > 25 ? `${game.title.slice(0, 20)}...` : game.title}</Text>
+        <Text style={[styles.cardText, {fontWeight: '600', fontSize: 24}]}>{game.title.length > 20 ? `${game.title.slice(0, 20)}...` : game.title}</Text>
         <View style={styles.cardPriceContainer}>
           <View style={styles.cardPrice}>
             {!(game.offer && game.price) && (
